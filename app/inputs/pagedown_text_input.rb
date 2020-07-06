c16317a3d2a4000492d0b47c7a9e2f0164d3330b
Riccardo Agatea <riccardo.agatea@gmail.com>
2020-07-06 17:26:29 +0200

    fix tag choice

--- app/inputs/pagedown_text_input.rb
+++ app/inputs/pagedown_text_input.rb
@@ -1,24 +1,21 @@
+# frozen_string_literal: true
+
 class PagedownTextInput < Formtastic::Inputs::StringInput
-
-
   def input_html_options
-    super.merge(:class => "wmd-input")
+    super.merge(class: 'wmd-input')
   end
 
   def to_html
     input_wrapping do
-
       label_html <<
-
-          template.content_tag(:div, :class => 'wmd-panel' ) do
-            template.content_tag(:div, "", :class => 'wmd-button-bar') <<
-                builder.text_area(method, input_html_options)
-          end <<
-
-          template.content_tag(:div, "", :style=> 'margin-top:20px' ) do
-            template.content_tag(:label, "Preview" ) <<
-                template.content_tag(:div, "", :class => "wmd-panel wmd-preview" )
-          end
+        template.content_tag(:div, class: 'wmd-panel') do
+          template.content_tag(:div, '', class: 'wmd-button-bar') <<
+            builder.text_area(method, input_html_options)
+        end <<
+        template.content_tag(:div, '', style: 'margin-top:20px') do
+          template.content_tag(:p, 'Preview', class: 'label wmd-preview-label') <<
+            template.content_tag(:div, '', class: 'wmd-panel wmd-preview')
+        end
     end.html_safe
   end
 end
